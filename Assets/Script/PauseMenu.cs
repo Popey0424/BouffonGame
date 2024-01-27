a1a9e9d090f440c8eca1973892f9b5c1de4b1fc4
P3D\Louis.Roy <roylouis0424@gmail.com>
2024-01-27 05:16:19 +0100

    Je push parce que mon pc a crash mdrr

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject MenuPause;
+    [SerializeField] private Image ImageFade;
+
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            MenuPause.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void OnClickResume()
+    {
+        Time.timeScale = 1f;
+        MenuPause.SetActive(false);
+    }
+
+    public void OnClickLeave()
+    {
+        ImageFade.DOFade(1, 2.9f).OnComplete(FadeComplete);
+        Time.timeScale = 1f;
+    }
+
+    public void FadeComplete()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnClickExit()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+}
