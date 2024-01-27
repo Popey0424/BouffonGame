f3b887a89612e96ffacaa7640962f737f30c690d
haruo59 <144923377+haruo59@users.noreply.github.com>
2024-01-27 19:25:41 +0100

    Menu sprite + son (j'ai pas réussi je suis nul)

--- Assets/Script/Menu/MainMenu.cs
+++ Assets/Script/Menu/MainMenu.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
+
 {
+    public AudioMixer Mixer;
+    public AudioSource Music;
+   
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,5 +19,24 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+
     }
+
+    public void OnMusicValueChanged(float newValue)
+    {
+        Music.volume = newValue;
+    }
+
+    //public void OnSFXValuechanged(float newValue)
+    //{
+    //    if (newValue < 0.01f)
+    //        newValue = 0.01f;
+
+    //    float volume = Mathf.Log10(newValue) * 20;
+
+    //    Mixer.SetFloat("SFX_Volumle", volume);
+    //}
+
 }
+
+
--- Assets/Script/SliderValue.cs
+++ Assets/Script/SliderValue.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
 
 public class SliderValue : MonoBehaviour
 {
     public TMP_Text TextValue;
     public Slider Slider;
-
+    
 
     public void OnValueChanged(float newValue)
     {
         int valueInt = (int)Mathf.Round(newValue * 100.0f);
         TextValue.text = valueInt.ToString();
     }
+
+    //public void OnSFXValuechanged(float newValue)
+    //{
+    //    if (newValue < 0.01f)
+    //        newValue = 0.01f;
+
+    //    float volume = Mathf.Log10(newValue) * 20;
+
+    //    Mixer.SetFloat("SFX_Volumle", volume);
+    //}
 }
