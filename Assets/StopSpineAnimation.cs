efef052bc415b8f882e87c4ac7af094866183b7b
poaups <theo.rotsart@gmail.com>
2024-01-28 05:00:27 +0100

    Spine LEo fini

--- Assets/StopSpineAnimation.cs
+++ Assets/StopSpineAnimation.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 using Spine.Unity;
+using JetBrains.Annotations;
+using Unity.Collections;
+using System.Collections;
 
 public class StopStartSpineAnimation : MonoBehaviour
 {
     private SkeletonAnimation skeletonAnimation;
+    public GameObject ImageUne;
+    public GameObject ImageDeux;
+    public GameObject ImageTrois;
     private MeshRenderer meshRenderer;
+    public float timer;
 
     void Start()
     {
@@ -24,21 +31,55 @@ public class StopStartSpineAnimation : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F1))
         {
             meshRenderer.enabled = false;
+            ImageUne.SetActive(true);
+            ImageDeux.SetActive(false);
+            ImageTrois.SetActive(false);
+            StartCoroutine(F1());
         }
 
         if (Input.GetKeyDown(KeyCode.F2))
         {
             meshRenderer.enabled = false;
+            ImageUne.SetActive(false);
+            ImageDeux.SetActive(true);
+            ImageTrois.SetActive(false);
+            StartCoroutine(F1());
         }
-
+           
         if (Input.GetKeyDown(KeyCode.F3))
         {
             meshRenderer.enabled = false;
+            ImageUne.SetActive(false);
+            ImageDeux.SetActive(false);
+            ImageTrois.SetActive(true);
+            StartCoroutine(F1());
+
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             meshRenderer.enabled = true;
+            ImageUne.SetActive(false);
+            ImageDeux.SetActive(false);
+            ImageTrois.SetActive(false);
         }
+
+
+
+
     }
+
+    IEnumerator F1()
+    {
+
+        yield return new WaitForSeconds(timer);
+        meshRenderer.enabled = true;
+        ImageUne.SetActive(false);
+        ImageDeux.SetActive(false);
+        ImageTrois.SetActive(false);
+
+
+    }
+    
+
 }
